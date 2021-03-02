302e108e09a9ad20aac9881908c503dc4060470d
Rinat Kamaletdinov <sky2208@ya.ru>
2021-03-02 13:08:27 +0400

    upload

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+// TODO: write your code here
+import Game, {GameSavingData, readGameSavin as loadGame, writeGameSaving as saveGame } from "./game.js"; 
+const game = new Game();
+game.start();
---
+++ src/js/game.js
@@ -0,0 +1,20 @@
+import './domain.js';
+
+export default class Game {
+    start() {
+        console.log('game started')
+    }
+}
+
+export class GameSavingData {
+
+}
+
+export function readGameSavin() {
+
+}
+
+export function writeGameSaving() {
+
+}
+
